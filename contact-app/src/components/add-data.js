71f16e0f9b7d5fec7124297e4853c7f1b626a6e9
Mohd Shahzaib Hussain <shahzaib.hussain@optimiser.com>
2025-05-21 23:32:21 +0530

    changes

--- contact-app/src/components/add-data.js
+++ contact-app/src/components/add-data.js
@@ -1,84 +1,9 @@
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import { RenderForm, sentenceCase, showError, showSuccess, showWarning } from "../contexts/common";
-import { defaultFields } from "../constant";
+import { showError, showSuccess } from "../contexts/common";
 import api from "../api/server";
 import { useSchema } from "../contexts/SchemaContext";
-
-class AddDataClass extends React.Component {
-    constructor(props) {
-        super(props);
-
-        // Find current tab's field config
-        const tabItem = props.schemaList[props.state.type];
-        const fields = tabItem?.fields || defaultFields;
-
-        // Initialize state based on field names
-        const initialState = {};
-        fields.forEach(f => { initialState[f.name] = ''; });
-
-        this.state = initialState;
-        this.fields = fields;
-    }
-
-    redirectToPreviousPage = () => {
-        const { state, navigate } = this.props;
-        navigate(
-            // `/${state.type ? `welcome/${state.username}` : 'getalldata/Contact'}`,
-            `/getalldata/${sentenceCase(state.type)}`,
-            { state: { type: state.type, collection: `${sentenceCase(state.type)}` } }
-        );
-    };
-
-    handleChange = (e) => {
-        const { name, value, refName, refValue } = e.target;
-        this.setState({ [name]: value });
-
-        if (refName) {
-            this.setState({ [refName]: refValue });
-        }
-    };
-
-    handleCancel = () => {
-        this.redirectToPreviousPage();
-    };
-
-    handleSave = async (e) => {
-        e.preventDefault();
-
-        // Check for any empty required fields
-        for (let field of this.fields) {
-            if (field.required && !this.state[field.name]) {
-                showWarning('All the fields are mandatory.');
-                return;
-            }
-        }
-
-        const dataToSave = { ...this.state };
-
-        this.fields.forEach(field => {
-            if (field.type === 'select' && field.ref && this.state[`${field.name}_RefFields`]) {
-                dataToSave[`${field.name}_RefFields`] = this.state[`${field.name}_RefFields`]
-            }
-        });
-
-        const response = await this.props.addDataHandler(dataToSave, sentenceCase(this.props.state.type));
-
-        if (response === 'success') {
-            const clearedState = {};
-            this.fields.forEach(f => clearedState[f.name] = '');
-            this.setState(clearedState);
-
-            this.redirectToPreviousPage();
-        }
-    };
-
-    render() {
-        return (
-            <RenderForm title={`Add ${sentenceCase(this.props.state.type)}`} buttonLabel="Add" self={this} />
-        );
-    }
-}
+import FormDataClass from "./form-data";
 
 // Wrapper to use hooks with class component
 const AddData = (props) => {
@@ -102,7 +27,7 @@ const AddData = (props) => {
         }
     };
 
-    return <AddDataClass addDataHandler={addDataHandler} schemaList={schemaList} navigate={useNavigate()} state={{ username, location, type }} />;
+    return <FormDataClass mode="add" addDataHandler={addDataHandler} schemaList={schemaList} navigate={useNavigate()} state={{ username, location, type }} />;
 };
 
 export default AddData;
---
+++ contact-app/src/components/form-data.js
@@ -0,0 +1,80 @@
+import React from "react";
+import { RenderForm, sentenceCase, showWarning } from "../contexts/common";
+import { defaultFields } from "../constant";
+
+class FormDataClass extends React.Component {
+    constructor(props) {
+        super(props);
+
+        // Find current tab's field config
+        const tabItem = props.schemaList[props.state.type];
+        const fields = tabItem?.fields || defaultFields;
+
+        // Initialize state based on field names
+        const initialState = {};
+        fields.forEach(f => { initialState[f.name] = ''; });
+
+        this.state = initialState;
+        this.fields = fields;
+    }
+
+    redirectToPreviousPage = () => {
+        const { state, navigate } = this.props;
+        navigate(
+            // `/${state.type ? `welcome/${state.username}` : 'getalldata/Contact'}`,
+            `/getalldata/${sentenceCase(state.type)}`,
+            { state: { type: state.type, collection: `${sentenceCase(state.type)}` } }
+        );
+    };
+
+    handleChange = (e) => {
+        const { name, value, refName, refValue } = e.target;
+        this.setState({ [name]: value });
+
+        if (refName) {
+            this.setState({ [refName]: refValue });
+        }
+    };
+
+    handleCancel = () => {
+        this.redirectToPreviousPage();
+    };
+
+    handleSave = async (e) => {
+        e.preventDefault();
+
+        // Check for any empty required fields
+        for (let field of this.fields) {
+            if (field.required && !this.state[field.name]) {
+                showWarning('All the fields are mandatory.');
+                return;
+            }
+        }
+
+        const dataToSave = { ...this.state };
+
+        this.fields.forEach(field => {
+            if (field.type === 'select' && field.ref && this.state[`${field.name}_RefFields`]) {
+                dataToSave[`${field.name}_RefFields`] = this.state[`${field.name}_RefFields`]
+            }
+        });
+
+        const response = await this.props.addDataHandler(dataToSave, sentenceCase(this.props.state.type));
+
+        if (response === 'success') {
+            const clearedState = {};
+            this.fields.forEach(f => clearedState[f.name] = '');
+            this.setState(clearedState);
+
+            this.redirectToPreviousPage();
+        }
+    };
+
+    render() {
+        return (
+            <RenderForm title={`Add ${sentenceCase(this.props.state.type)}`} buttonLabel="Add" self={this} />
+        );
+    }
+}
+
+export default FormDataClass;
