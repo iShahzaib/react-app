e3521218a3790b1230f0071304fa79dd56c6ad2c
Mohd Shahzaib Hussain <shahzaib.hussain@optimiser.com>
2025-05-28 19:55:12 +0530

    changes

--- contact-app/src/components/build-list.js
+++ contact-app/src/components/build-list.js
@@ -16,14 +16,15 @@ const BuildList = React.memo(({ type, origin }) => {
 
     const [listData, setListData] = useState([]);
     const [selectedIds, setSelectedIds] = useState([]);
-    // const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(false);
 
     const { username: loggedInUsername } = localStorage.getItem("loggedInUser") ? JSON.parse(localStorage.getItem("loggedInUser")) : {};
 
     const retrieveData = useCallback(async () => {
         try {
-            // setLoading(true);
+            setLoading(true);
             setSelectedIds([]);
+            setListData([]);
             // const getData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
             const response = await api.get(`/api/getdocdata?collection=${sentenceCase(type)}`);
 
@@ -38,7 +39,7 @@ const BuildList = React.memo(({ type, origin }) => {
         } catch (err) {
             console.error("Error fetching data:", err);
         } finally {
-            // setLoading(false);
+            setLoading(false);
         }
     }, [type, setListData]);
 
@@ -99,42 +100,40 @@ const BuildList = React.memo(({ type, origin }) => {
 
     return (
         <div className="ui main container">
-            {/* {loading ? (
+            <HeaderNav
+                type={type}
+                tab={schema}
+                filteredData={filteredData}
+                loggedInUsername={loggedInUsername}
+                origin={origin}
+            />
+            <SearchBar
+                type={type}
+                tab={schema}
+                searchTerm={searchTerm}
+                setSearchTerm={setSearchTerm}
+                selectedIds={selectedIds}
+                setSelectedIds={setSelectedIds}
+                retrieveData={retrieveData}
+                deleteObjects={deleteObjects}
+            />
+            {loading ? (
                 <div className="ui inline fallback-loader">
                     <div className="ui text active loader small">Loading...</div>
                 </div>
             ) : (
-                <> */}
-                    <HeaderNav
-                        type={type}
-                        tab={schema}
-                        filteredData={filteredData}
-                        loggedInUsername={loggedInUsername}
-                        origin={origin}
-                    />
-                    <SearchBar
-                        type={type}
-                        tab={schema}
-                        searchTerm={searchTerm}
-                        setSearchTerm={setSearchTerm}
-                        selectedIds={selectedIds}
-                        setSelectedIds={setSelectedIds}
-                        retrieveData={retrieveData}
-                        deleteObjects={deleteObjects}
-                    />
-                    <GridTable
-                        type={type}
-                        fields={fields}
-                        filteredData={filteredData}
-                        loggedInUsername={loggedInUsername}
-                        deleteObjects={deleteObjects}
-                        isAllSelected={isAllSelected}
-                        toggleSelectAll={toggleSelectAll}
-                        toggleSelectOne={toggleSelectOne}
-                        selectedIds={selectedIds}
-                    />
-                {/* </>
-            )} */}
+                <GridTable
+                    type={type}
+                    fields={fields}
+                    filteredData={filteredData}
+                    loggedInUsername={loggedInUsername}
+                    deleteObjects={deleteObjects}
+                    isAllSelected={isAllSelected}
+                    toggleSelectAll={toggleSelectAll}
+                    toggleSelectOne={toggleSelectOne}
+                    selectedIds={selectedIds}
+                />
+            )}
         </div>
     );
 });
